dde6f07882ebfa95b9c14787b380df144b596683
Thien Duong <thienduong@kms-technology.com>
2021-11-28 22:00:08 +0700

    add logic user Manager

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,19 @@
 const Response = require("../helpers/response.helper");
 const remove_Id = require("../utils/remove_Id");
+const constant = require("../constants/index");
+
+const User = require("../models/User");
+
+const uploadImage = require("../utils/uploadImage");
+
+const {
+  response: {
+    createSuccessMessage,
+    updateSuccessMessage,
+    deleteSuccessMessage,
+    failMessage,
+  },
+} = require("../constants");
 
 exports.getAll = async (req, res, next) => {
   const { _limit, _page, q } = req.query;
@@ -31,20 +45,53 @@ exports.getAll = async (req, res, next) => {
 };
 
 exports.update = async (req, res, next) => {
-  const {
-    params: { userId },
-    body: {
-      email,
-      firstName,
-      lastName,
-      // fullName,
-      phoneNumber,
-      address,
-      birthday,
-      gender,
-    },
+  let {
+    file,
+    body: { firstName, lastName, phoneNumber, address, birthday, gender },
+    user,
   } = req;
+  try {
+    if (
+      !user
+      // !firstName &&
+      // !lastName
+      // phoneNumber &&
+      // address &&
+      // birthday &&
+      // !(gender === "true" || gender === "false")
+    )
+      throw new Error(failMessage);
 
+    let obj = {
+      // firstName,
+      // lastName,
+    };
+
+    if (firstName)
+      obj = { ...obj, firstName, fullName: `${firstName} ${user.lastName}` };
+    if (lastName)
+      obj = { ...obj, lastName, fullName: `${user.firstName} ${lastName}` };
+    if (phoneNumber) obj = { ...obj, phoneNumber };
+    if (address) obj = { ...obj, address };
+    if (birthday) obj = { ...obj, birthday: new Date(birthday) };
+    if (gender === "true" || gender === "false")
+      obj = { ...obj, gender: gender === "true" };
+
+    if (file) {
+      const result = await uploadImage(file);
+      obj = { ...obj, avatar: result.url };
+    }
+
+    user = await User.findByIdAndUpdate(user._id, { ...obj });
+    user._doc.id = user._id;
+
+    return Response.success(res, { message: updateSuccessMessage, user });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+exports.updatePassword = async (req, res, next) => {
   try {
   } catch (error) {
     return next(error);
