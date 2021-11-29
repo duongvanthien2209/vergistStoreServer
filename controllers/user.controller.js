34d3c94f69c6ac3e26075e006f49e2da473e811b
Thien Duong <thienduong@kms-technology.com>
2021-11-29 08:16:23 +0700

    add logic for delete

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,6 +3,9 @@ const remove_Id = require("../utils/remove_Id");
 const constant = require("../constants/index");
 
 const User = require("../models/User");
+const Cart = require("../models/Cart");
+const CartDetail = require("../models/CartDetail");
+const Bill = require("../models/Bill");
 
 const uploadImage = require("../utils/uploadImage");
 
@@ -14,7 +17,9 @@ const {
     failMessage,
   },
 } = require("../constants");
+const BillDetail = require("../models/BillDetail");
 
+// Find Users
 exports.getAll = async (req, res, next) => {
   const { _limit, _page, q } = req.query;
 
@@ -44,6 +49,7 @@ exports.getAll = async (req, res, next) => {
   }
 };
 
+// Update User Information
 exports.update = async (req, res, next) => {
   let {
     file,
@@ -91,6 +97,7 @@ exports.update = async (req, res, next) => {
   }
 };
 
+// Update Password - Send Code To Gmail
 exports.updatePassword = async (req, res, next) => {
   try {
   } catch (error) {
@@ -98,4 +105,44 @@ exports.updatePassword = async (req, res, next) => {
   }
 };
 
-exports.delete = async (req, res, next) => {};
+exports.delete = async (req, res, next) => {
+  const {
+    params: { userId },
+  } = req;
+
+  try {
+    if (!userId) throw new Error(failMessage);
+    const user = await User.findById(userId);
+    if (!user) throw new Error(failMessage);
+
+    // Xóa tất cả liên quan tới User trong CSDL
+
+    // - Giỏ hàng - Chi tiết giỏ hàng
+    const cart = await Cart.findOne({ userId: user._id });
+    if (!cart) throw new Error(failMessage);
+    const cartDetails = await CartDetail.find({ cartId: cart._id });
+    for (let cartDetail of cartDetails)
+      await CartDetail.findByIdAndDelete(cartDetail._id);
+    await Cart.findByIdAndDelete(cart._id);
+
+    // - Hóa đơn - Chi tiết hóa đơn
+    const bills = await Bill.find({ userId: user._id });
+    if (!bills) throw new Error(failMessage);
+    for (let bill of bills) {
+      const billDetails = await BillDetail.find({ billId: bill._id });
+      for (let billDetail of billDetails)
+        await BillDetail.findByIdAndDelete(billDetail._id);
+      await Bill.findByIdAndDelete(bill._id);
+    }
+
+    // - Bình Luận - Đánh giá
+
+    // - Mã giảm giá
+
+    await User.findByIdAndDelete(userId);
+
+    return Response.success(res, { message: deleteSuccessMessage });
+  } catch (error) {
+    return next(error);
+  }
+};
