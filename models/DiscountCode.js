34a2d437689d18d5e4792de5da77cf9b0a019e7c
Thien Duong <thienduong@kms-technology.com>
2021-12-12 08:50:10 +0700

    add Schema DiscountCode,DiscountCodeDetail

---
+++ models/DiscountCode.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const DiscountCodeSchema = new mongoose.Schema({
+  codeName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+  sale: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  amount: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  dateCreate: {
+    type: Date,
+    default: Date.now(),
+    required: true,
+  },
+  dateExpire: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model(
+  "DiscountCode",
+  DiscountCodeSchema,
+  "discountCodes"
+);
